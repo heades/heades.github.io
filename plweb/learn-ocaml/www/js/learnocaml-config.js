e9d2585ea501489dfe1e99f376394bf102b28c43
Harley D. Eades III <harley.eades@gmail.com>
2023-01-16 15:31:44 -0500

    Updating PL Website.

---
+++ plweb/learn-ocaml/www/js/learnocaml-config.js
@@ -0,0 +1,8 @@
+var learnocaml_config = {
+  enableTutorials: false,
+  enablePlayground: false,
+  enableLessons: false,
+  enableExercises: true,
+  enableToplevel: true,
+  baseUrl: "https://hde.design/plweb/learn-ocaml"
+}
